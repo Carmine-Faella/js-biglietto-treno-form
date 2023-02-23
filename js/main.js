0a7b035c492fd10deb1549d72a918009fb9d02d1
Carmine Faella <carminefaella5@gmail.com>
2023-02-23 16:29:59 +0100

    Da abbellire e migliorare e implementare

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 
 const fasciaEta = document.getElementById("fasciaEta");
 
-const prezzo = document.getElementById("price");
-
 const nomeCog = document.getElementById("nomeCog");
 
-const numeroDiKm = document.getElementById("numeroKm");
+const price = document.getElementById("price");
 
-/*
+const carrozza = document.getElementById("carrozza")
+
+const codiceCp = document.getElementById("codiceCp")
+
+const numeroDiKm = document.getElementById("numeroKm").value;
 
 let prezzoBi = 0.21;
 
@@ -17,15 +19,18 @@ let prezzoMinore = (prezzoTot - (prezzoTot * (20/100)));
 
 let prezzoOver = (prezzoTot - (prezzoTot * (40/100)));
 
-*/
-
 const domButton = document.querySelector('#leggiForm');
 
 domButton.addEventListener('click', 
     function () {
         const nomeDom = document.getElementById('nomeCognome');
         const nomeDelMessaggio = nomeDom.value;
-        nomeCog.innerHTML = nomeDelMessaggio;  
+        const numeroCasuale = Math.floor((Math.random() * 6)+1);
+        carrozza.innerHTML = numeroCasuale
+        const numeroCasualeCp = Math.floor((Math.random() * 99000));
+        codiceCp.innerHTML = numeroCasualeCp
+        nomeCog.innerHTML = nomeDelMessaggio; 
+        price.innerHTML = prezzoTot;
     }
 );
 
@@ -35,11 +40,10 @@ domReset.addEventListener('click',
     function() {
         const nomeDom = document.getElementById('nomeCognome');
         nomeDom.value = "";
-    }
+        const numeDom = document.getElementById('numeroKm')
+        numeDom.value = ""
+    } 
 );
 
 
-console.log(prezzoTot)
-
-console.log(numeroDiKm)
 
