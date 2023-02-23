b48747f43555692ad473322a32a48ac1813083ff
Carmine Faella <carminefaella5@gmail.com>
2023-02-23 19:32:04 +0100

    Incompleto

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-
-const fasciaEta = document.getElementById("fasciaEta");
-
+ 
 const nomeCog = document.getElementById("nomeCog");
 
-const price = document.getElementById("price");
+const prezzo = document.getElementById("price");
 
 const carrozza = document.getElementById("carrozza")
 
@@ -11,9 +9,11 @@ const codiceCp = document.getElementById("codiceCp")
 
 const numeroDiKm = document.getElementById("numeroKm").value;
 
+const fasciaEta = document.getElementById("fasciaEta").value;
+
 let prezzoBi = 0.21;
 
-let prezzoTot = numeroDiKm * prezzoBi;
+let prezzoTot = numeroDiKm.value * prezzoBi;
 
 let prezzoMinore = (prezzoTot - (prezzoTot * (20/100)));
 
@@ -22,15 +22,14 @@ let prezzoOver = (prezzoTot - (prezzoTot * (40/100)));
 const domButton = document.querySelector('#leggiForm');
 
 domButton.addEventListener('click', 
-    function () {
+    function (){
         const nomeDom = document.getElementById('nomeCognome');
         const nomeDelMessaggio = nomeDom.value;
-        const numeroCasuale = Math.floor((Math.random() * 6)+1);
-        carrozza.innerHTML = numeroCasuale
-        const numeroCasualeCp = Math.floor((Math.random() * 99000));
-        codiceCp.innerHTML = numeroCasualeCp
         nomeCog.innerHTML = nomeDelMessaggio; 
-        price.innerHTML = prezzoTot;
+        const numeroCasuale = Math.floor((Math.random() * 6)+1);
+        carrozza.innerHTML = numeroCasuale;
+        const numeroCasualeCp = Math.floor((Math.random() * 99000));
+        codiceCp.innerHTML = numeroCasualeCp;
     }
 );
 
@@ -40,8 +39,6 @@ domReset.addEventListener('click',
     function() {
         const nomeDom = document.getElementById('nomeCognome');
         nomeDom.value = "";
-        const numeDom = document.getElementById('numeroKm')
-        numeDom.value = ""
     } 
 );
 
